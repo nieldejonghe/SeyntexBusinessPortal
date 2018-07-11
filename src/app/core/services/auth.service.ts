8d97b5aa73bee2f1943921808011a8746bb706fa
nieldejonghe <nieldejonghe@hotmail.com>
2018-07-11 11:49:07 +0200

    Navbar and login changes

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -3,8 +3,7 @@ import { Observable} from 'rxjs';
 import { ApiService } from './api.service';
 import { User } from '../models';
 import { map } from 'rxjs/operators';
-
-
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -16,40 +15,55 @@ export class AuthService {
   // URL where to fetch users
   private usersUrl = 'api/users/login';  // URL to web api
 
+
+  //BehaviourSubject holds the value, used to change menu items on navbar when login/logout
+  //making the observable here so it doesn't create a new observable in the method that returns this observable
+  private loggedInSubj = new BehaviorSubject<boolean>(false);
+
+  private loggedIn$ = this.loggedInSubj.asObservable()
+
+
   constructor(private api: ApiService) { }
-
-
 
   //login user (checks if user with id and pw exists?
   login(username: string, password: string) {
+
   return this.api.post(this.usersUrl, {username: username, password: password})
     .pipe(map( (user) => {
         if (user && user.token) {
           console.log("token stored" + user.token);
+          //adds user to local storage so user keeps session in between browsing pages
           localStorage.setItem('loggedUser', JSON.stringify(user));
+          this.loggedInSubj.next(this.getUserSession());
         }
         return user // Return for further chaining
       })
     )
-
-
-//GIMMEH USER PLX
-
-    //if there is a user returned and user has token store details and jwt token in local storage to keep user logged in between pages
-    //if(this.user && this.user.token){
-      //console.log(this.user.token);
-      //localStorage.setItem('loggedUser', JSON.stringify(user));
-    //}
-  //})
-
   }
 
-
-
-  //logout by removing user from local storage ?
+  //logout by removing user from local storage
   logout(){
-    localStorage.removeItem('currentUser');
+    localStorage.removeItem('loggedUser');
+    this.loggedInSubj.next(this.getUserSession());
   }
+
+  //getting usersession
+  // '!!' = casting to boolean
+  getUserSession(): boolean{
+    return !!localStorage.getItem('loggedUser');
+
+  }
+
+  //method to return state Behavioursubject loggedin, used to change menu items on navbar when login /logout
+
+  //don't return asObservable since this will create a new observable everytime you call this method
+  isLoggedin(){
+    return this.loggedIn$
+
+  }
+
+
+
 
 }
 
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService} from '../core/services';
+import {Observable} from 'rxjs';
 
 @Component({
   selector: 'app-navbar',
@@ -7,9 +9,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+
+  loggedin$: Observable<boolean>;
+
+  constructor(private authservice: AuthService) { }
 
   ngOnInit() {
+    console.log("test");
+    this.loggedin$ = this.authservice.isLoggedin();
+    console.log(this.loggedin$);
   }
 
 }
