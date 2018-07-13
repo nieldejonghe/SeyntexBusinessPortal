3271394a20a1da435de4b6903e781656b3ac2f20
nieldejonghe <nieldejonghe@hotmail.com>
2018-07-13 11:22:08 +0200

    created login form with some css

--- src/app/userdashboard/userdashboard.component.ts
+++ src/app/userdashboard/userdashboard.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {AuthService, OrderService} from '../core/services';
 import {User, Order} from '../core/models';
-import {forkJoin} from 'rxjs';
-import { map, flatMap } from 'rxjs/operators'
 
 @Component({
   selector: 'app-userdashboard',
@@ -10,10 +8,8 @@ import { map, flatMap } from 'rxjs/operators'
   styleUrls: ['./userdashboard.component.css']
 })
 export class UserdashboardComponent implements OnInit {
-
   user: User;
   orders: Order[];
-
   constructor(private authservice: AuthService, private orderservice: OrderService) { }
 
   ngOnInit() {
@@ -22,13 +18,12 @@ export class UserdashboardComponent implements OnInit {
     //this.orderservice.getOrderbyUserId(this.user.id).subscribe((order_info: Order) => this.order = order_info);
     //this.getOrders()
 
-    //this works sincs getCurrentUser is a sync call
+    //this works sincs getCurrentUser is a sync call, gets the currentlogged in user observable
     this.user = this.authservice.getCurrentUser();
+
+    //get all orders from current logged in user and put them in array
     this.orderservice.getOrderbyUserId(this.user.id).subscribe((order_info: Order[]) => this.orders = order_info);
-
-
   }
-
-
-
+  //parse into readable strings
+  parseOrders(){}
 }
